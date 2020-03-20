f7459e09e2152915aea18a8637bce248262df070
Nishanth Solomon <nsolomo2@asu.edu>
2020-03-20 00:30:40 -0700

    Modified code to get candidates

--- textual_entailment.py
+++ textual_entailment.py
@@ -1,16 +1,34 @@
 from allennlp.predictors.predictor import Predictor
 
 
-predictor = Predictor.from_path("./models/decomposable-attention-elmo-2018.02.19.tar.gz")
-prediction = predictor.predict(
-  hypothesis="The elephant was lost.",
-  premise="A large, gray elephant walked beside a herd of zebras."
-)
+class TextualEntailment():
+    def __init__(self, config):
+        model_path = config['model_path']
 
-entailment = prediction['label_probs'][0]
-contradiction = prediction['label_probs'][1]
-neutral = prediction['label_probs'][2]
+        self.predictor = Predictor.from_path(model_path)
 
-print('entailment : ' + str(entailment))
-print('contradiction : ' + str(contradiction))
-print('neutral : ' + str(neutral))
+    def get_entailment_score(self, hypothesis, premise):
+        prediction = self.predictor.predict(
+            hypothesis=hypothesis,
+            premise=premise
+        )
+
+        entailment_score = prediction['label_probs'][0]
+        contradiction_score = prediction['label_probs'][1]
+        neutral_score = prediction['label_probs'][2]
+
+        return entailment_score
+
+    def get_entailment_candidate_list(self, question, elasticsearch_candidates):
+        candidates = []
+        for elasticsearch_candidate in elasticsearch_candidates:
+            entailment_score = self.get_entailment_score(
+                question, elasticsearch_candidate)
+            candidates.append((elasticsearch_candidate, entailment_score))
+
+        sorted(candidates, key=lambda x: (-x[1]))
+
+        candidates = [candidate[0] for candidate in candidates]
+
+        candidates = candidates[:10]
+        return candidates
