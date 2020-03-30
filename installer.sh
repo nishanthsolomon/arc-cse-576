01ce36b83f38ad36055bb15594540a5165bb3607
Nishanth Solomon <nsolomo2@asu.edu>
2020-03-30 16:57:05 -0700

    Modified installer.sh

--- installer.sh
+++ installer.sh
@@ -1,21 +1,56 @@
 #!/bin/bash
 
-CUR_DIR=`pwd`
 
-while read line; do 
-    directory_to_save="$(echo $line | cut -d',' -f1)"
-    file_name="$(echo $line | cut -d',' -f2)"
-    download_url="$(echo $line | cut -d',' -f3)"
-    file_path=${directory_to_save}/${file_name}
-    cd $CUR_DIR
-    if [ -e $file_path ]
+export LC_ALL="en_US.UTF-8"
+
+main()
+{
+    set -e
+    SetVariables
+    execution
+}
+SetVariables()
+{
+    CUR_DIR=`pwd`
+    MODEL_DIR=${CUR_DIR}/models
+    DATASET_DIR=${CUR_DIR}/dataset
+
+    TEXTUAL_ENTAILMENT_PATH=${MODEL_DIR}/decomposable-attention-elmo-2018.02.19.tar.gz
+    TEXTUAL_ENTAILMENT_URL=https://s3-us-west-2.amazonaws.com/allennlp/models/decomposable-attention-elmo-2018.02.19.tar.gz
+    
+    ARC_DATASET_PATH=${DATASET_DIR}/ARC-V1-Feb2018
+    ARC_DATASET_URL=https://s3-us-west-2.amazonaws.com/ai2-website/data/ARC-V1-Feb2018.zip
+
+}
+
+
+execution()
+{
+    #Download Models
+
+    if [ -e ${TEXTUAL_ENTAILMENT_PATH} ]
     then
-        echo "${file_name} OK."
+        echo "TEXTUAL ENTAILMENT MODEL OK."
     else
-        mkdir -p -- "$directory_to_save"
-        cd $directory_to_save
-        wget $download_url
-        echo "Downloaded ${file_name}"
+        mkdir -p -- "${DATASET_DIR}"
+        cd ${DATASET_DIR}
+        wget $TEXTUAL_ENTAILMENT_URL
+        echo "Downloaded TEXTUAL ENTAILMENT MODEL"
     fi
 
-done < downloadables.txt
+    #Download Dataset
+
+    if [ -e ${ARC_DATASET_PATH} ]
+    then
+        echo "ARC DATASET OK."
+    else
+        mkdir -p -- "${DATASET_DIR}"
+        cd ${DATASET_DIR}
+        wget $ARC_DATASET_URL
+        unzip ARC-V1-Feb2018.zip
+        echo "Downloaded ARC DATASET"
+    fi
+
+}
+
+main $*
